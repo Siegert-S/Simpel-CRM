7719981966015529d05654d8483b3776acc51598
Sascha Siegert <s.siegert85@googlemail.com>
2025-03-20 12:35:08 +0100

    add 'add user' at user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatSidenavModule],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatSidenavModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
